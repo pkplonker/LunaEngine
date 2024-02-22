0d5cd2af6df64632e4083d0008b95bd7b574152d
stuart <pkplonker@gmail.com>
2024-02-22 17:33:59 +0000

    Transform

---
+++ LunaEngine/Engine/Transform.cs
@@ -0,0 +1,61 @@
+﻿using System.Numerics;
+
+namespace Engine;
+
+public class Transform
+{
+	private bool isDirty = true;
+
+	private Vector3 position = new Vector3(0f);
+
+	public Vector3 Position
+	{
+		get => position;
+		set
+		{
+			position = value;
+			isDirty = true;
+		}
+	}
+
+	private Vector3 scale = new Vector3(1f);
+
+	public Vector3 Scale
+	{
+		get => scale;
+		set
+		{
+			scale = value;
+			isDirty = true;
+		}
+	}
+
+	private Quaternion rotation = Quaternion.Identity;
+
+	public Quaternion Rotation
+	{
+		get => rotation;
+		set
+		{
+			rotation = value;
+			isDirty = true;
+		}
+	}
+
+	private Matrix4x4 viewMatrix;
+
+	public Matrix4x4 ViewMatrix
+	{
+		get
+		{
+			if (isDirty)
+			{
+				viewMatrix = Matrix4x4.Identity * Matrix4x4.CreateFromQuaternion(Rotation) *
+				             Matrix4x4.CreateScale(Scale) * Matrix4x4.CreateTranslation(Position);
+				isDirty = false;
+			}
+
+			return viewMatrix;
+		}
+	}
+}
