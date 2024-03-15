a92496c28c6642552debfc221350b8bc5be3c08e
stuart <pkplonker@gmail.com>
2024-03-15 16:42:27 +0000

    Added base panel

---
+++ LunaEngine/Editor/IMGui/IMGUIControls/Panels/ContentBrowser.cs
@@ -0,0 +1,17 @@
+﻿using Engine;
+using ImGuiNET;
+
+namespace Editor.Controls
+{
+	public class ContentBrowser : IPanel
+	{
+		public string PanelName { get; set; } = "Content Browser";
+
+		public void Draw(IRenderer renderer)
+		{
+			ImGui.Begin(PanelName);
+			ImGui.Text("Testing");
+			ImGui.End();
+		}
+	}
+}
